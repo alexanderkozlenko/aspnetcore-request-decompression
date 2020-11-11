ad5d685ec714a418950ace073495ba846aa2493e
Alexander Kozlenko <alexander.kozlenko@live.com>
2020-11-11 20:53:28 +0200

    Upgrade to .NET 5.0

--- src/Anemonis.AspNetCore.RequestDecompression/RequestDecompressionLoggerExtensions.cs
+++ src/Anemonis.AspNetCore.RequestDecompression/RequestDecompressionLoggerExtensions.cs
@@ -13,17 +13,17 @@ namespace Anemonis.AspNetCore.RequestDecompression
         private static readonly Action<ILogger, Type, Exception> s_logRequestDecodingApplied =
             LoggerMessage.Define<Type>(
                 LogLevel.Debug,
-                new EventId(1100, "REQDEC_DECODING_APPLIED"),
+                new(1100, "REQDEC_DECODING_APPLIED"),
                 Strings.GetString("logging.decoding_applied"));
         private static readonly Action<ILogger, Exception> s_logRequestDecodingSkipped =
             LoggerMessage.Define(
                 LogLevel.Debug,
-                new EventId(1101, "REQDEC_DECODING_SKIPPED"),
+                new(1101, "REQDEC_DECODING_SKIPPED"),
                 Strings.GetString("logging.decoding_skipped"));
         private static readonly Action<ILogger, Exception> s_logRequestDecodingDisabled =
             LoggerMessage.Define(
                 LogLevel.Warning,
-                new EventId(1300, "REQDEC_DECODING_DISABLED"),
+                new(1300, "REQDEC_DECODING_DISABLED"),
                 Strings.GetString("logging.decoding_disabled"));
 
         public static void LogRequestDecodingApplied(this ILogger logger, Type type)
